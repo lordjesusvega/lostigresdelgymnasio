b2d05c1d30eeafc0f24d2db59ca75b206deb63ba
Jesus Vega <jesusvega@Jesuss-MacBook-Pro.local>
2022-07-28 13:12:32 -0700

    Placeholder image

---
+++ src/Nav.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function Nav() {
+  return (
+    <div className="Nav">
+      <h1 className="nav-title">LOS TIGRES DEL GYMNASIO</h1>
+    </div>
+  );
+}
+
+export default Nav;
