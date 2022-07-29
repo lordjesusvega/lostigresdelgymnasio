18c1f05d0ae7eeb7f1ce522113b4c9ecc760e6c1
Jesus Vega <jesusvega@Jesuss-MacBook-Pro.local>
2022-07-28 22:49:20 -0700

    blck and white hero images

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ function App() {
   return (
     <div className="App">
       <Nav/>
-      <img className="heroImage" src="https://live.staticflickr.com/65535/52246927042_16c3a397e5_b.jpg"/>
+      <div className="hero-image-container">
+        <img className="heroImage one" src="https://live.staticflickr.com/65535/52246927042_16c3a397e5_b.jpg"/>
+        <img className="heroImage" src="https://live.staticflickr.com/65535/52246927042_16c3a397e5_b.jpg"/>
+        <img className="heroImage three" src="https://live.staticflickr.com/65535/52246927042_16c3a397e5_b.jpg"/>
+      </div>
     </div>
   );
 }
